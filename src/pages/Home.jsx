9fedfa6d8a563a6f5cd209f1fc3721c9d4212448
kavitasingh06 <kvsingh2321@gmail.com>
2026-06-25 17:19:49 +0530

    Increase logo size to 350px

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,9 +26,9 @@ const Home = () => {
       <main>
         <HeroSection />
         <section style={{ backgroundColor: '#ffffff', padding: '3rem 1rem', display: 'flex', justifyContent: 'center', gap: '4rem', flexWrap: 'wrap', alignItems: 'center' }}>
-          <img src="/pic1.png" alt="Highlight 1" style={{ height: '200px', width: 'auto', maxWidth: '100%', objectFit: 'contain' }} />
-          <img src="/changepic1.png" alt="Highlight 2" style={{ height: '200px', width: 'auto', maxWidth: '100%', objectFit: 'contain' }} />
-          <img src="/changepic2.png" alt="Highlight 3" style={{ height: '200px', width: 'auto', maxWidth: '100%', objectFit: 'contain' }} />
+          <img src="/pic1.png" alt="Highlight 1" style={{ height: '350px', width: 'auto', maxWidth: '100%', objectFit: 'contain' }} />
+          <img src="/changepic1.png" alt="Highlight 2" style={{ height: '350px', width: 'auto', maxWidth: '100%', objectFit: 'contain' }} />
+          <img src="/changepic2.png" alt="Highlight 3" style={{ height: '350px', width: 'auto', maxWidth: '100%', objectFit: 'contain' }} />
         </section>
         <Features />
         <WhyChooseVGU />
