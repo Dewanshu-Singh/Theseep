678299ec9a69402ae62d017438ce5eef82a0af63
kavitasingh06 <kvsingh2321@gmail.com>
2026-06-25 17:31:22 +0530

    Further reduce wrapper heights to crop even more transparent space

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,13 +26,13 @@ const Home = () => {
       <main>
         <HeroSection />
         <section style={{ backgroundColor: '#ffffff', padding: '0', display: 'flex', justifyContent: 'center', gap: '2rem', flexWrap: 'wrap', alignItems: 'center', overflow: 'hidden' }}>
-          <div style={{ height: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', width: '28%', minWidth: '250px' }}>
+          <div style={{ height: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', width: '28%', minWidth: '250px' }}>
             <img src="/pic1.png" alt="Highlight 1" style={{ height: '280px', width: 'auto', maxWidth: 'none', objectFit: 'contain' }} />
           </div>
-          <div style={{ height: '120px', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', width: '32%', minWidth: '280px' }}>
+          <div style={{ height: '80px', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', width: '32%', minWidth: '280px' }}>
             <img src="/changepic1.png" alt="Highlight 2" style={{ height: '320px', width: 'auto', maxWidth: 'none', objectFit: 'contain' }} />
           </div>
-          <div style={{ height: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', width: '28%', minWidth: '250px' }}>
+          <div style={{ height: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', width: '28%', minWidth: '250px' }}>
             <img src="/changepic2.png" alt="Highlight 3" style={{ height: '280px', width: 'auto', maxWidth: 'none', objectFit: 'contain' }} />
           </div>
         </section>
